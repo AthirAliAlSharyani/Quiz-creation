717172be5a50cc48f352d2c41992a5794e8349cd
aathirr <athiralsharyani@gmail.com>
2024-04-24 16:22:40 +0400

    add question page and related components

--- quiz-hub/src/app/layout.tsx
+++ quiz-hub/src/app/layout.tsx
@@ -1,8 +1,8 @@
 "use client";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+
 import "./globals.css";
-import useQuiz from "./store";
+import { Inter } from "next/font/google";
+import { configType, useQuiz } from "./store";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
   quiz: React.ReactNode;
 }) {
-  const config = useQuiz((state) => state.config);
+  const config = useQuiz((state: any) => state.config);
   let render = config.status ? quiz : children;
   return (
     <html lang="en">
